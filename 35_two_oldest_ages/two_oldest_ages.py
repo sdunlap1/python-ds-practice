0fe459c28763a2856413fd5c6c800db865bd08d3
sdunlap1 <sdunlap1@mac.com>
2024-03-25 07:55:18 -0700

    Completed Practice 35 thru 39

--- 35_two_oldest_ages/two_oldest_ages.py
+++ 35_two_oldest_ages/two_oldest_ages.py
@@ -13,6 +13,8 @@ def two_oldest_ages(ages):
         >>> two_oldest_ages([1, 5, 5, 2])
         (2, 5)
     """
+    oldest_ages = sorted(set(ages))
+    return tuple(oldest_ages[-3:])
 
     # NOTE: don't worry about an optimized runtime here; it's fine if
     # you have a runtime worse than O(n)
